6b4e16cdef054bfd7d8d2648b67fe87fd8349366
antonm <antonm@ca.ibm.com>
2017-06-13 16:16:21 -0400

    Solves #18 - roughed in views

--- tonememo/LoginButtonStyle.swift
+++ tonememo/LoginButtonStyle.swift
@@ -13,13 +13,13 @@ class LoginButtonStyle: UIButton {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
-        layer.shadowOpacity = 0.8
-        layer.shadowRadius = 5.0
-        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+//        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+//        layer.shadowOpacity = 0.8
+//        layer.shadowRadius = 5.0
+//        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         layer.cornerRadius = 6.0
-        layer.borderWidth = 0.25
-        layer.borderColor = UIColor(red: 0, green: 0, blue: 80/255, alpha: 1).cgColor
+//        layer.borderWidth = 0.25
+//        layer.borderColor = UIColor(red: 0, green: 0, blue: 80/255, alpha: 1).cgColor
     }
 
 }
